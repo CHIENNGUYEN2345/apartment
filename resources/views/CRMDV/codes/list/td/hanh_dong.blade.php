42ff4427c280606ca9d981c30e7be37c08280200
Hoang Hung <hoanghung.developer@gmail.com>
2023-12-18 14:50:11 +0700

    update

--- resources/views/CRMDV/codes/list/td/hanh_dong.blade.php
+++ resources/views/CRMDV/codes/list/td/hanh_dong.blade.php
@@ -1,6 +1,8 @@
+@if(in_array($module['code'] . '_edit', $permissions) || in_array('super_admin', $permissions))
 <span class="edit"><a
             href="{{ url('/admin/'.$module['code'].'/edit/' . $item->id) }}"
             title="Sửa bản ghi này">{{trans('admin.edit')}}</a> | </span>
+@endif
 {{--    <span class=""><a--}}
 {{--                href="{{ url('/admin/'.$module['code'].'/' . $item->id . '/duplicate') }}"--}}
 {{--                title="Nhân bản bản ghi này">Nhân bản</a> | </span>--}}
@@ -9,4 +11,6 @@
                            href="{{ url('/admin/'.$module['code'].'/delete/' . $item->id) }}"
                            title="Xóa bản ghi">{{trans('admin.delete')}}</a> | </span>
 @endif
-<a href="/admin/bao_cao_dan_khach/add?code_id={{ $item->id }}">Báo cáo dẫn khách</a>
+@if(in_array('bao_cao_dan_khach_add', $permissions) || in_array('super_admin', $permissions))
+<a href="/admin/bao_cao_dan_khach/add?code_id={{ $item->id }}" style="padding: 5px; border: 1px solid #ccc;">Báo cáo dẫn khách</a>
+@endif
