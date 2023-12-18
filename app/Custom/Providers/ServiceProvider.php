38f5c1afa4e251869ac03aac4b43b594001f1db2
Hoang Hung <hoanghung.developer@gmail.com>
2023-12-18 15:04:02 +0700

    upadte

--- app/Custom/Providers/ServiceProvider.php
+++ app/Custom/Providers/ServiceProvider.php
@@ -51,33 +51,19 @@ class ServiceProvider
     public function addSetting()
     {
         \Eventy::addFilter('setting.custom_module', function ($module) {
-            $module['tabs']['web_service'] = [
-                'label' => 'Dịch vụ web',
+
+
+            $module['tabs']['cham_cong_tab'] = [
+                'label' => 'Cấu hình chấm công',
                 'icon' => '<i class="flaticon2-time"></i>',
                 'td' => [
-                    ['name' => 'inner', 'type' => 'inner', 'label' => '', 'html' => '====|Quá|======|Bây giờ|=====|Sát|======|Sắp đến|======>'],
-                    ['name' => 'min_day', 'type' => 'number', 'label' => 'Số ngày thông báo quá kì hạn'],
-                    ['name' => 'close_day', 'type' => 'number', 'label' => 'Số ngày thông báo sát kì hạn'],
-                    ['name' => 'max_day', 'type' => 'number', 'label' => 'Số ngày thông báo sắp đến kì hạn'],
-                    ['name' => 'inner', 'type' => 'inner', 'label' => '', 'html' => '<b>Cấu hình gia hạn tự động</b>'],
-                    ['name' => 'status', 'type' => 'checkbox', 'label' => 'Kích hoạt gửi thông báo tự động'],
-                    ['name' => 'minute_scan', 'type' => 'text', 'label' => 'Phút (0-59) tương ứng với số từ (0-59)', 'des' => 'Nhập vào số phút, có thể nhập vào 2 giá trị các nhau bởi dấu phảy :<br> Ví dụ ( phút 20 và tháng 50 ) : 20, 50'],
-                    ['name' => 'hour_scan', 'type' => 'text', 'label' => 'Giờ (0-23) tương ứng với số từ (0-23)', 'des' => 'Nhập vào số tháng, có thể nhập vào 2 giá trị các nhau bởi dấu phảy'],
-                    ['name' => 'day_in_month_scan', 'type' => 'text', 'label' => 'Ngày trong tháng (1-31) tương ứng với số từ (1-31)', 'des' => 'Nhập vào số ngày trong tháng, có thể nhập vào 2 giá trị các nhau bởi dấu phảy'],
-                    ['name' => 'month_scan', 'type' => 'text', 'label' => 'Tháng (1-12) tương ứng với số từ (1-12)', 'des' => 'Nhập vào số tháng, có thể nhập vào 2 giá trị các nhau bởi dấu phảy'],
-                    ['name' => 'day_in_week_scan', 'type' => 'text', 'label' => 'Thứ trong tuần ( thứ 2 -> Chủ nhật tương ứng với số từ 0 -> 7)', 'des' => 'Nhập vào số giờ, có thể nhập vào 2 giá trị các nhau bởi dấu phảy (chủ nhật = 0 or 7)'],
+                    ['name' => 'cham_cong_xa_toi_da', 'type' => 'number', 'label' => 'Khoảng cách chấm công tối đa (m)'],
+                    ['name' => 'vp_long', 'type' => 'text', 'label' => 'Toạ độ: Lat'],
+                    ['name' => 'vp_lat', 'type' => 'text', 'label' => 'Toạ độ: Long'],
                 ]
             ];
 
-            $module['tabs']['check_link_error'] = [
-                'label' => 'Kiểm tra lỗi web tự động',
-                'icon' => '<i class="flaticon2-time"></i>',
-                'td' => [
-                    ['name' => 'links_direct', 'type' => 'textarea', 'class' => 'form-action', 'label' => 'Danh sách tên miền chuyển hướng', 'inner' => 'rows=15'],
-                    ['name' => 'links', 'type' => 'textarea', 'class' => 'form-action', 'label' => 'DS Link theo dõi thêm', 'inner' => 'rows=15'],
-                    ['name' => 'ignore_links', 'type' => 'textarea', 'class' => 'form-action', 'label' => 'DS Link loại trừ', 'inner' => 'rows=15'],
-                ]
-            ];
+
             return $module;
         }, 1, 1);
     }
